a83e606040c017dc0f1a8698c4f4c0bbc738b4fc
Michael Straughan <mstraughan86@gmail.com>
2017-09-10 20:00:00 -0700

    ECMA 6 Upgrade

--- scripts/quickstart.js
+++ scripts/quickstart.js
@@ -1,39 +1,25 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+const fs = require('fs');
+const readline = require('readline');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-var SCOPES =
-  [
-    'https://www.googleapis.com/auth/drive',
-    'https://www.googleapis.com/auth/drive.file',
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/tagmanager.delete.containers',
-    'https://www.googleapis.com/auth/tagmanager.edit.containers',
-    'https://www.googleapis.com/auth/tagmanager.edit.containerversions',
-    'https://www.googleapis.com/auth/tagmanager.manage.accounts',
-    'https://www.googleapis.com/auth/tagmanager.manage.users',
-    'https://www.googleapis.com/auth/tagmanager.publish',
-  ];
-
-var SPREADSHEET_ID = '1OoSOjnw9gSsxhpiH9XZz9YLWuqCjrBo1XY-rnmaV6pg'; //1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
-var RANGE = 'Sheet1!A2:O';
-
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'token.googleapis.com-nodejs-quickstart.json';
-
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
-});
+const SCOPES = [
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/tagmanager.delete.containers',
+  'https://www.googleapis.com/auth/tagmanager.edit.containers',
+  'https://www.googleapis.com/auth/tagmanager.edit.containerversions',
+  'https://www.googleapis.com/auth/tagmanager.manage.accounts',
+  'https://www.googleapis.com/auth/tagmanager.manage.users',
+  'https://www.googleapis.com/auth/tagmanager.publish'
+];
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'token.googleapis.com-nodejs-quickstart.json';
+const SPREADSHEET_ID = '1OoSOjnw9gSsxhpiH9XZz9YLWuqCjrBo1XY-rnmaV6pg'; //1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms
+const RANGE = 'Sheet1!A2:O';
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -42,12 +28,12 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+const authorize = (credentials, callback) => {
+  const clientSecret = credentials.installed.client_secret;
+  const clientId = credentials.installed.client_id;
+  const redirectUrl = credentials.installed.redirect_uris[0];
+  const auth = new googleAuth();
+  const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, function (err, token) {
@@ -58,7 +44,7 @@ function authorize(credentials, callback) {
       callback(oauth2Client);
     }
   });
-}
+};
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -68,13 +54,13 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback to call with the authorized
  *     client.
  */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
+const getNewToken = (oauth2Client, callback) => {
+  const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
   });
   console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
@@ -90,14 +76,14 @@ function getNewToken(oauth2Client, callback) {
       callback(oauth2Client);
     });
   });
-}
+};
 
 /**
  * Store token to disk be used in later program executions.
  *
  * @param {Object} token The token to store to disk.
  */
-function storeToken(token) {
+const storeToken = (token) => {
   try {
     fs.mkdirSync(TOKEN_DIR);
   } catch (err) {
@@ -107,14 +93,14 @@ function storeToken(token) {
   }
   fs.writeFile(TOKEN_PATH, JSON.stringify(token));
   console.log('Token stored to ' + TOKEN_PATH);
-}
+};
 
 /**
  * Print the names and majors of students in a sample spreadsheet:
  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
-function listMajors(auth) {
-  var sheets = google.sheets('v4');
+const listMajors = (auth) => {
+  const sheets = google.sheets('v4');
   sheets.spreadsheets.values.get({
     auth: auth,
     spreadsheetId: SPREADSHEET_ID,
@@ -124,16 +110,31 @@ function listMajors(auth) {
       console.log('The API returned an error: ' + err);
       return;
     }
-    var rows = response.values;
+    const rows = response.values;
     if (rows.length == 0) {
       console.log('No data found.');
     } else {
       console.log('Tag Name, CSS Selector, GTM ID:');
-      for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
         // Print columns A and E, which correspond to indices 0 and 4.
         console.log('%s, %s, %s', row[4], row[8], row[10]);
       }
     }
   });
-}
+};
+
+const main = () => {
+  // Load client secrets from a local file.
+  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+    authorize(JSON.parse(content), listMajors);
+  });
+};
+
+main();
