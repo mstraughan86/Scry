f6d1c8528fae45575959310952850c6e212e821e
Michael Straughan <mstraughan86@gmail.com>
2017-11-27 20:00:00 -0700

    Script to activate MongoDB from CLI for development/troubleshooting. (Not for production)

--- scripts/database.js
+++ scripts/database.js
@@ -1,3 +1,10 @@
-/**
- * Created by Michael Straughan on 9/30/2017.
- */
+const mongoose = require('../utilities/mongoose.js');
+
+// This was to initialize MongoDB in a separate process in order to facilitate
+// proper database functionality.
+// This appears to be deprecated, as proper shutdown timing avoids this need.
+const main = () => {
+  mongoose.initialize();
+};
+
+main();
