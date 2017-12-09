85ddcfdc124e61aee31b5f83b418f20034e44e41
Michael Straughan <mstraughan86@gmail.com>
2017-12-08 20:00:00 -0700

    Removing dataDictionary from openGraph.js, which requires me to actually finish the openGraph implementation. Upcoming will be the open-graph.dust template as well as the importVideoData.js to include better data resolution.

--- utilities/openGraph.js
+++ utilities/openGraph.js
@@ -1,67 +1,74 @@
+const path = require('path');
+const fs = require('fs');
+const mongoose = require(path.join(__dirname, '..', 'utilities', 'mongoose.js'));
+
 const dataDictionary = require('../utilities/dataDictionary.js');
 
-var ogExample = {
+// gonna get rid of this because we wont ever use it.
+// examples be damned.
+// the video.episode example below is going to be the one
+let ogExample = {
   'facebookAppId': 'ccc',
   'images': [
-  {
-    'openGraph-image': 'https://github.com/edent/SuperTinySocialIcons/blob/master/original/github.png',
-    'openGraph-imageSecureUrl': 'https://github.com/edent/SuperTinySocialIcons/blob/master/original/github.png',
-    'openGraph-imageType': 'image/png',
-    'openGraph-imageWidth': '200',
-    'openGraph-imageHeight': '200',
-    'openGraph-imageAltText': 'imageAltText'
-  },
-  {
-    'openGraph-image': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/flickr.png',
-    'openGraph-imageSecureUrl': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/flickr.png',
-    'openGraph-imageType': 'image/png',
-    'openGraph-imageWidth': '200',
-    'openGraph-imageHeight': '200',
-    'openGraph-imageAltText': 'imageAltText'
-  },
-  {
-    'openGraph-image': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/twitter.png',
-    'openGraph-imageSecureUrl': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/twitter.png',
-    'openGraph-imageType': 'image/png',
-    'openGraph-imageWidth': '200',
-    'openGraph-imageHeight': '200',
-    'openGraph-imageAltText': 'imageAltText'
-  }
-],
+    {
+      'openGraph-image': 'https://github.com/edent/SuperTinySocialIcons/blob/master/original/github.png',
+      'openGraph-imageSecureUrl': 'https://github.com/edent/SuperTinySocialIcons/blob/master/original/github.png',
+      'openGraph-imageType': 'image/png',
+      'openGraph-imageWidth': '200',
+      'openGraph-imageHeight': '200',
+      'openGraph-imageAltText': 'imageAltText'
+    },
+    {
+      'openGraph-image': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/flickr.png',
+      'openGraph-imageSecureUrl': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/flickr.png',
+      'openGraph-imageType': 'image/png',
+      'openGraph-imageWidth': '200',
+      'openGraph-imageHeight': '200',
+      'openGraph-imageAltText': 'imageAltText'
+    },
+    {
+      'openGraph-image': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/twitter.png',
+      'openGraph-imageSecureUrl': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/twitter.png',
+      'openGraph-imageType': 'image/png',
+      'openGraph-imageWidth': '200',
+      'openGraph-imageHeight': '200',
+      'openGraph-imageAltText': 'imageAltText'
+    }
+  ],
   'openGraph-type': "video.episode",
   'openGraph-videoSeries': 'asdf',
   'openGraph-videoReleaseDate': 'asdf',
   'openGraph-videoDuration': 'asdf',
   'openGraph-videoTags': 'asdf',
   'actors': [
-  {
-    'link': 'https://www.imdb.com/1234',
-    'role': 'Norm Hull'
-  },
-  {
-    'link': 'https://www.imdb.com/5678',
-    'role': 'Todd Margarette'
-  }
-],
+    {
+      'link': 'https://www.imdb.com/1234',
+      'role': 'Norm Hull'
+    },
+    {
+      'link': 'https://www.imdb.com/5678',
+      'role': 'Todd Margarette'
+    }
+  ],
   'directors': [
-  {
-    'link': 'https://www.imdb.com/1234'
-  }
-],
+    {
+      'link': 'https://www.imdb.com/1234'
+    }
+  ],
   'writers': [
-  {
-    'link': 'https://www.imdb.com/1234'
-  }
-]
+    {
+      'link': 'https://www.imdb.com/1234'
+    }
+  ]
 };
 
+// ** potential omni-tool
 // function renameKeys(obj, newKeys) {
 //   return Object.keys(obj).map(key => {
 //     const newKey = newKeys[key] || key;
 //     return { [newKey]: obj[key] };
 //   });
 // }
-
 // const renameKeys = (obj, newKeys) => Object.keys(obj).map(k => {return { [(newKeys[k] || k)]: obj[k] };});
 
 function openGraphConstructor(color, seating, fuel) {
@@ -70,29 +77,56 @@ function openGraphConstructor(color, seating, fuel) {
   this.fuelConsumption = fuel;
 }
 
-const generate = (pageType, pageObject = 'default') => {
+const generate = (pageType, pageObject) => {
   if (pageType == 'home') {
     return ogExample;
   }
   if (pageType == 'video') {
-    const videoObject = dataDictionary.getVideoData(pageObject);
-    videoObject.series = dataDictionary.getSeries();
-    return {
-      title: videoObject.title,
-      description: videoObject.description,
-      images: videoObject.images,
+    // in addition to normalizing this
+    // i must also go into open-graph.dust and normalize those expected parameters
+    // the ogExample above is just confusing because now we are mixing two naming
+    // conventions. get it together mike.
 
-      type: 'video.episode',
+    return mongoose.schemas.VideoObject
+      .find({'title': pageObject})
+      .collation({locale: 'en', strength: 2})
+      .sort({season: 1})
+      .sort({episode: 1})
+      .then(result => {
+        const videoObject = result[0];
 
-      series: videoObject.series,
-      releaseDate: videoObject.releaseDate,
-      duration: videoObject.duration,
-      tags: videoObject.tags,
+        var images = videoObject.forEach(video => {
+          return {
+            'url': 'https://raw.githubusercontent.com/edent/SuperTinySocialIcons/master/original/flickr.png',
+            'type': 'image/png', // this is a png
+            'width': '200', // i dont have this
+            'height': '200', // i dont have this
+            'alt': 'imageAltText' // i dont have this
+          }
+        });
 
-      actors: videoObject.actors,
-      directors: videoObject.directors,
-      writers: videoObject.writers
-    };
+        return {
+          // these 3 are defaults
+          title: videoObject.title,
+          description: videoObject.description,
+          images: videoObject.images, // make into an array somehow
+
+          // type definer
+          type: 'video.episode',
+
+          // video episode specific stuff.
+          series: videoObject.series,
+          releaseDate: videoObject.releaseDate,
+          duration: videoObject.duration,
+          tags: videoObject.tags,
+
+          actors: videoObject.actors,
+          directors: videoObject.directors,
+          writers: videoObject.writers
+        };
+
+      })
+      .catch(err => {console.log('ERR: app.post /video/playlist', err)});
   }
   if (pageType == 'page') {
 
